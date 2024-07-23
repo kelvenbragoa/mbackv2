36547f1993f10c5fbc451372b76b886aead4edc7
Kelven Bragoa <kelvenbragoa@hotmail.com>
2024-07-23 20:08:06 +0200

    fix: remove sancthum

--- app/Http/Controllers/Api/UserAuthController.php
+++ app/Http/Controllers/Api/UserAuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserAuthController extends Controller
@@ -16,18 +17,25 @@ class UserAuthController extends Controller
             'email'=>'required|string|email',
             'password'=>'required|min:8'
         ]);
-        $user = User::where('email',$loginUserData['email'])->first();
-        if(!$user || !Hash::check($loginUserData['password'],$user->password)){
+
+        if (Auth::attempt($loginUserData)) {
+            $user = User::where('email',$loginUserData['email'])->first();
+            $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+            return response()->json([
+                'user'=>$user,
+                'access_token' => $token,
+            ]);
+        }else{
             return response()->json([
                 'message' => 'Usuario/Password incorrectos'
             ],401);
         }
+        // $user = User::where('email',$loginUserData['email'])->first();
+        // if(!$user || !Hash::check($loginUserData['password'],$user->password)){
+           
+        // }
             
-        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
-        return response()->json([
-            'user'=>$user,
-            'access_token' => $token,
-        ]);
+        
     }
 
     public function register(Request $request){
