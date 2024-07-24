ee8161b2cce56418f881171a7d2f14984cd40d9b
Kelven Bragoa <kelvenbragoa@hotmail.com>
2024-07-24 11:25:30 +0200

    fix auth

--- app/Http/Middleware/Sanctum.php
+++ app/Http/Middleware/Sanctum.php
@@ -16,7 +16,7 @@ class Sanctum
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    public function handle(Request $request, Closure $next)
+    public function handle(Request $request, Closure $next) : Response
     {
         $bearer = $request->bearerToken();
 
