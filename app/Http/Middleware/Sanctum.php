bce41ac41a4dae17418d8bde4648a1613205d4ed
Kelven Bragoa <kelvenbragoa@hotmail.com>
2024-07-25 12:12:28 +0200

    sancthm

--- app/Http/Middleware/Sanctum.php
+++ app/Http/Middleware/Sanctum.php
@@ -38,9 +38,8 @@ class Sanctum
         $url = explode("|", $tokenWithBearer);
         $id = $url[1];
 
-        $instance = DB::table('personal_access_tokens')->find($id);
+        $instance = DB::table('personal_access_tokens')->where('token',$id)->first();
 
-        return $instance;
 
         
 
