0873f9ea40a0d2125ce21b740f4962a0f5f379f8
Kelven Bragoa <kelvenbragoa@hotmail.com>
2024-10-19 08:13:01 +0200

    timezone change

--- config/app.php
+++ config/app.php
@@ -66,7 +66,7 @@ return [
     */
 
     // 'timezone' => env('APP_TIMEZONE', 'UTC'),
-    'timezone'=>'Africa/Maputo',
+    'timezone'=>'Africa/Johannesburg',
 
     /*
     |--------------------------------------------------------------------------
